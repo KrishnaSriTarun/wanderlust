50ea6a55f4e05a10da49ff2e621da0638fc060b4
Krishna <krishnasritarun95@gmail.com>
2024-12-25 09:51:24 +0530

    phone number error solved

--- models/user.js
+++ models/user.js
@@ -2,31 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = new Schema({
-      username: {
-            type: String,
-            required: true,
-            unique: true,
+const userSchema = new Schema(
+      {
+            username: {
+                  type: String,
+                  required: true,
+                  unique: true,
+            },
+            email: {
+                  type: String,
+                  required: true,
+                  unique: true, 
+                  match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+            },
+            phoneNumber: {
+                  type: String,
+                  required: true,
+                  match: [/^\d{10}$/, 'Please fill a valid phone number'], 
+            },
+            displayName: {
+                  type: String,
+                  default: '',
+            },
+            photoURL: {
+                  type: String,
+                  default: '',
+            },
       },
-      email: {
-            type: String,
-            required: true,
-            unique: true, // Ensure unique email addresses
-            match: [/.+\@.+\..+/, 'Please fill a valid email address'], 
-      },
-      phoneNumber: {
-            type: String,
-      },
-      displayName: {
-            type: String,
-            default: '', 
-      },
-      photoURL: {
-            type: String,
-            default: '', 
-      },
-      }, 
-      { timestamps: true });
+      { timestamps: true }
+);
+
+// Middleware to normalize phoneNumber
+userSchema.pre('save', function (next) {
+      if (this.phoneNumber.startsWith('0')) {
+            this.phoneNumber = this.phoneNumber.slice(1); 
+      }
+      next();
+});
 
 userSchema.plugin(passportLocalMongoose);
 
