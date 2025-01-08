a1260f62c091e019bbff6f9c804a4e7eb330ccea
Krishna <krishnasritarun95@gmail.com>
2025-01-08 11:21:42 +0530

    mobile navbar also corrected

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema(
-      {
+      { 
             username: {
                   type: String,
                   required: true,
