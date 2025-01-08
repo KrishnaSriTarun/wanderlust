68ad9bbb906a3f8ceec3d033e9151ee82e6b9915
Krishna <krishnasritarun95@gmail.com>
2025-01-08 11:36:21 +0530

    map style changed

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
       container : 'map', // container ID
-      style:'mapbox://styles/mapbox/dark-v11', // style URL
+      style:'mapbox://styles/mapbox/navigation-night-v1', // style URL
       center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
       zoom: 9
 });
